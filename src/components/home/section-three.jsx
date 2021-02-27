c607c6f260cce433569cda205717f487dc125a3d
KristofferLeemburg <kristofferleemburg@gmail.com>
2021-02-27 20:34:51 +0100

    footer changes

--- src/components/home/section-three.jsx
+++ src/components/home/section-three.jsx
@@ -6,9 +6,9 @@ export default function Section() {
     return (
         <div className="home">
             <div className="section-three-container">
-                <div className="text-box-left"></div>
-                <div className="text-box-middle"></div>
-                <div className="text-box-right"></div>
+                <div className="text-box-left">ddd</div>
+                <div className="text-box-middle">ddd</div>
+                <div className="text-box-right">ddd</div>
             </div>
         </div>
     );
