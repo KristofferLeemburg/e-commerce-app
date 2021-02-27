03a61dbe74928775a9ef07d097459dd972081544
KristofferLeemburg <kristofferleemburg@gmail.com>
2021-02-27 12:51:56 +0100

    added scss styles folder

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,15 +1,8 @@
-import React, { useState } from 'react';
-
+import React from 'react';
 export default function Navigation() {
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
-
+ 
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
     </div>
   );
 }
