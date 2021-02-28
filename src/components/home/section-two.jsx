017c821b19c5bd6a78e912d903021f122d8ebc4b
KristofferLeemburg <kristofferleemburg@gmail.com>
2021-02-28 10:42:17 +0100

    media querie adjustments

--- src/components/home/section-two.jsx
+++ src/components/home/section-two.jsx
@@ -6,13 +6,16 @@ export default function Section() {
     return (
         <div className="home">
             <div className="section-container">
-                <p className="section1">I’m calling time on the
-                idea that you’re broken.
-                It’s bullshit.
+                <p className="section1">Rust en Balans in je leven,
+                <ul>
+                    <li>Heb jij weinig energie</li>
+                    <li>Zit je niet goed in je vel</li>
+                    <li>Voel jij je onrustig</li>
+                    <li>Wil je eigenlijk iets anders, maar wat?</li>
+                </ul>
                 </p>
-                <p className="section2">There are whole industries dedicated to telling you you’re broken.
-                You need fixing. You’ll only be good enough when you see a certain number in your clothes or on the scales.
-                That “success” means having a certain job, looking a certain way and living a certain lifestyle.
+                <p className="section2">Ik ben ontzettend dankbaar dat jij hier terecht bent gekomen! Als lifestylecoach, die tevens een echte ervaringsdeskundige is, begrijp ik als geen ander waar je tegenaan loopt: “been there, done That”!!
+                Maar wat als ik je je vertel dat jij net als ik beschik over onbegrensde mogelijkheden om jou leven in eigen handen te nemen? Dat jij echt het leven kunt leven zoals jij dat wilt! En ik wil graag samen met jou die weg uitstippelen.
                 </p>
             </div>
         </div>
