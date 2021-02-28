87ecda1e2d04b5fe2308248332902da24c0d450c
KristofferLeemburg <kristofferleemburg@gmail.com>
2021-02-28 10:04:39 +0100

    npm run build

--- src/components/home/section-one.jsx
+++ src/components/home/section-one.jsx
@@ -1,29 +1,28 @@
 import React, {useEffect, useRef} from "react";
 import "../../styles/home.css"
-import {TimelineLite ,TweenMax, Power3} from 'gsap';
+import {TimelineLite, Power3} from 'gsap';
 
 
 import imgGirl from "../../images/Shannon-Wilkinson-3.png"
 
 function App() {
-  let app = useRef(null)
+
   let images = useRef(null)
   let tl = new TimelineLite({ delay: .6});
   
   useEffect(() => {
 
     const girlImage = images.firstElementChild;    
-    TweenMax.to(app, 0, {css: {visibility: 'visible'}})
 
     tl.from(girlImage, 1, {x: 2080, ease: Power3.easeOut},'Start')
-    .from(girlImage.firstElementChild, 2, {scale: 1, ease: Power3.easeOut}, .5)
+    .from(girlImage.firstElementChild, 2, {ease: Power3.easeOut}, .5)
   }, [tl])
 
 
 
   return (
     <div className="section-one-container">
-    <div className="hero" ref={el => app = el}>
+    <div className="hero">
           <div className="hero-images">
             <div ref={el => images = el}  className="hero-images-inner">
               <div className="hero-image girl">
